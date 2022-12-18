429d3ab49e75ffba7a4a26b828c6fada247c36e1
wanderson-rpf <wandersonrpf@gmail.com>
2022-12-18 18:41:23 -0300

    :construction: [progress] Criando pagina de registro e adicioando classes ao login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Login from './Page/Login';
+import Login from './page/Login';
+import Register from './page/Register';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Login />} />
+      <Route path='/' element={<Register />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
   );
 }
