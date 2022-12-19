64f63fce9d74d77e4fb32cb08b7c5429cf2acaf3
wanderson-rpf <wandersonrpf@gmail.com>
2022-12-19 20:45:05 -0300

    :construction: [progress] Criando menu.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { FaUserCircle } from 'react-icons/fa';
+import { GrFavorite } from 'react-icons/gr';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+import { FiLogOut } from 'react-icons/fi';
+import { Link } from "react-router-dom";
 
 function Menu() {
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className="container-menu">
+        <ul>
+          <li>
+            <Link>
+              <FaUserCircle />
+              <p>User</p>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <GrFavorite />
+              <p>Favorites</p>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <BsFillCheckCircleFill />
+              <p>Recipes Done</p>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <FiLogOut />
+              <p>Logout</p>
+            </Link>
+          </li>
+        </ul>
     </div>  
   );
 }
--- src/page/Meals.jsx
+++ src/page/Meals.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 function Meals() {
   return (
-    <div>
+    <div className="container-meals">
       <Header />
     </div>
   );
