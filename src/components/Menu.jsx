a31664e8f08843cb538fe4f2a4276cb59ea25a3a
wanderson-rpf <wandersonrpf@gmail.com>
2022-12-20 13:11:25 -0300

    :lipstick: [style] Style inicial do menu.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,36 +1,57 @@
 import React from "react";
 import { FaUserCircle } from 'react-icons/fa';
-import { GrFavorite } from 'react-icons/gr';
+import { MdFavorite } from 'react-icons/md';
 import { BsFillCheckCircleFill } from 'react-icons/bs';
 import { FiLogOut } from 'react-icons/fi';
+import { FaHome } from 'react-icons/fa';
+import { TbMeat } from 'react-icons/tb';
+import { BiDrink } from 'react-icons/bi';
 import { Link } from "react-router-dom";
 
 function Menu() {
   return (
     <div className="container-menu">
-        <ul>
+        <ul className="container-list-menu">
+        <li>
+            <Link to="/meals" className="list-items">
+              <FaHome className="icon-menu" />
+              <p>Home</p>
+            </Link>
+          </li>
           <li>
-            <Link>
-              <FaUserCircle />
+            <Link to="/profile" className="list-items">
+              <FaUserCircle className="icon-menu" />
               <p>User</p>
             </Link>
           </li>
           <li>
-            <Link>
-              <GrFavorite />
+            <Link to="/profile" className="list-items">
+              <TbMeat className="icon-menu" />
+              <p>Meals</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile" className="list-items">
+              <BiDrink className="icon-menu" />
+              <p>Drinks</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/favorite" className="list-items">
+              <MdFavorite className="icon-menu" />
               <p>Favorites</p>
             </Link>
           </li>
           <li>
-            <Link>
-              <BsFillCheckCircleFill />
+            <Link to="/recipe-done" className="list-items">
+              <BsFillCheckCircleFill className="icon-menu" />
               <p>Recipes Done</p>
             </Link>
           </li>
-          <li>
-            <Link>
-              <FiLogOut />
-              <p>Logout</p>
+          <li className="opt-logout">
+            <Link to="/" className="list-items">
+              <FiLogOut className="icon-menu" />
+              <p>Sign out</p>
             </Link>
           </li>
         </ul>
