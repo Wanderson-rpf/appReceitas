05b52c224290929df432baa661f1473459e6c92c
wanderson-rpf <wandersonrpf@gmail.com>
2023-01-23 22:15:31 -0300

    :construction: [progress] Inserindo btn favorito.

--- src/page/Favorites.jsx
+++ src/page/Favorites.jsx
@@ -1,11 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
+import { getDataLocalStorage } from "../services/localStorage";
+import { Link } from "react-router-dom";
+import ButtonShared from "../components/ButtonShared";
+import ButtonFavorite from "../components/ButtonFavorite";
 
 function Favorites() {
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const [allFavoriteRecipes, setAllFavoriteRecipes] = useState([]);
+
+  useEffect(() => {
+    const newFavoriteRecipes = getDataLocalStorage("favoriteRecipes");
+    setFavoriteRecipes(newFavoriteRecipes);
+    setAllFavoriteRecipes(newFavoriteRecipes);
+  }, []);
+
+  const handleFilterMeals = () => {
+    setFavoriteRecipes(allFavoriteRecipes);
+    const filtredMealsFavorite = favoriteRecipes.filter(
+      (favoriteRecipe) => favoriteRecipe.type === "meal"
+    );
+    setFavoriteRecipes(filtredMealsFavorite);
+  };
+
+  const handleFilterDrinks = () => {
+    setFavoriteRecipes(allFavoriteRecipes);
+    const filtredDrinksFavorite = favoriteRecipes.filter(
+      (favoriteRecipe) => favoriteRecipe.type === "drink"
+    );
+    setFavoriteRecipes(filtredDrinksFavorite);
+  };
+
   return (
     <div>
       <Header />
-      <h1>Favorites</h1>
+      <h1>Receitas Favoritas</h1>
+      <div>
+        <button
+          type="button"
+          onClick={() => setFavoriteRecipes(allFavoriteRecipes)}
+        >
+          Todos
+        </button>
+        <button type="button" onClick={handleFilterMeals}>
+          Pratos
+        </button>
+        <button type="button" onClick={handleFilterDrinks}>
+          Bebidas
+        </button>
+      </div>
+      <div>
+        {favoriteRecipes.map((recipeFav, index) => (
+          <div key={index}>
+            <div>
+              <Link
+                to={
+                  recipeFav.type === "meal"
+                    ? `/meals/${recipeFav.id}`
+                    : `/drinks/${recipeFav.id}`
+                }
+              >
+                <img src={recipeFav.thumb} alt="favorite.thumb" />
+              </Link>
+              <div>
+                <Link
+                  to={
+                    recipeFav.type === "meal"
+                      ? `/meals/${recipeFav.id}`
+                      : `/drinks/${recipeFav.id}`
+                  }
+                >
+                  <p>{recipeFav.name}</p>
+                </Link>
+                <p>{recipeFav.category}</p>
+                <div>
+                  <ButtonShared />
+                  <ButtonFavorite id={recipeFav.id} />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
