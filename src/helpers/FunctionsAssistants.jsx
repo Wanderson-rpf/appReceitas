e0371f5979415fc8e9b5baf377c0a00c4635e27c
wanderson-rpf <wandersonrpf@gmail.com>
2023-01-29 10:38:47 -0300

    :construction: [progress] Movendo funções para arquivo helpers.

--- src/helpers/FunctionsAssistants.jsx
+++ src/helpers/FunctionsAssistants.jsx
@@ -1,3 +1,43 @@
+export const createObjRecipe = (pageRecipe, recipe) => {
+  return {
+    id: pageRecipe === 'meals' ? recipe.idMeal : recipe.idDrink,
+    thumb: pageRecipe === 'meals' ? recipe.strMealThumb : recipe.strDrinkThumb,
+    page: pageRecipe === 'meals' ? 'meals' : 'drinks',
+    name: pageRecipe === 'meals' ? recipe.strMeal : recipe.strDrink,
+    category: recipe.strCategory
+  }
+}
+
+export const thumbRecipe = (page) => {
+  let strName;
+  if (page === "/meals") {
+    strName = "strMealThumb";
+  } else if (page === "/drinks") {
+    strName = "strDrinkThumb";
+  }
+  return strName;
+};
+
+export const strNameRecipe = (page) => {
+  let strName;
+  if (page === "/meals") {
+    strName = "strMeal";
+  } else if (page === "/drinks") {
+    strName = "strDrink";
+  }
+  return strName;
+};
+
+export const idVerify = (page) => {
+  let id;
+  if (page === '/meals') {
+    id = 'idMeal';
+  } else if (page === "/drinks")  {
+    id = 'idDrink';
+  }
+  return id;
+};
+
 export const listIngredients = (list) => {
   const ingredientsList = list.map((element) => Object.entries(element)
   .filter((elem) => elem[0].includes('strIngredient')
